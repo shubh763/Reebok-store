86a8bc3bc75d3c2851305c0b01f69186aaccd673
shubh763 <shubh252000@gmail.com>
2024-04-14 23:56:06 +0530

    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navigation from "./Component/Navigation";
+import HeroSection from "./Component/Hero";
+const App = () => {
+
+  return (
+    <div>
+
+      <Navigation />
+      <HeroSection />
+      
+    </div>
+  )
+}
+
+export default App;
